23222d5347c2ec78f872ebc6aadb183b2a7b7105
restimguay <resti.guay@gmail.com>
2018-12-01 23:20:29 +0800

    remove phpmango/phpmango from required list

---
+++ src/obj/BaseObject.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace obj;
+
+class BaseObject
+{
+    public function __get($name)
+    {
+        if(isset($this->$name)){
+            return $this->$name;
+        }
+        throw new \Exception('The property '.$name.'@'.__CLASS__.' does not exist');
+    }
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function __toString()
+    {
+        return '';
+    }
+}
