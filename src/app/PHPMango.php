797dca2fc843a9226d18234145b368e60e794c9b
restimguay <resti.guay@gmail.com>
2018-12-02 17:07:42 +0800

    added the main class ,PHPMango
    added the main class

---
+++ src/app/PHPMango.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app;
+
+class PHPMango
+{
+    public function __construct($basePath = '')
+    {
+        
+    }
+    public static function start(){
+        echo 'started';
+    }
+}
